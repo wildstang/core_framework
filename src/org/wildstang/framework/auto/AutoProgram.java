98072773e302d80cb923b6134f5e36dd4d4fe5a4
Steve Garward <garwards@gmail.com>
2017-03-15 09:37:59 -0500

    Added SmartDashboard out for current step.

--- src/org/wildstang/framework/auto/AutoProgram.java
+++ src/org/wildstang/framework/auto/AutoProgram.java
@@ -9,97 +9,120 @@ import org.wildstang.framework.auto.steps.AutoStep;
 import org.wildstang.framework.auto.steps.control.AutoStepStopAutonomous;
 import org.wildstang.framework.core.Core;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  *
  * @author Nathan
  */
-public abstract class AutoProgram {
+public abstract class AutoProgram
+{
 
-	protected final List<AutoStep> programSteps = new ArrayList<>();
-	protected int currentStep;
-	protected boolean finishedPreviousStep, finished;
+   protected final List<AutoStep> programSteps = new ArrayList<>();
+   protected int currentStep;
+   protected boolean finishedPreviousStep, finished;
    private static Logger s_log = Logger.getLogger(AutoProgram.class.getName());
 
-	protected abstract void defineSteps(); // Use this method to set the steps
-											// for this program. Programs
-											// execute the steps in the array
-											// programSteps serially.
-	// Remember to clear everything before all of your steps are finished,
-	// because once they are, it immediately drops into Sleeper.
+   protected abstract void defineSteps(); // Use this method to set the steps
+   // for this program. Programs
+   // execute the steps in the array
+   // programSteps serially.
+   // Remember to clear everything before all of your steps are finished,
+   // because once they are, it immediately drops into Sleeper.
 
-	public void initialize() {
-		defineSteps();
-		loadStopPosition();
-		currentStep = 0;
-		finishedPreviousStep = false;
-		finished = false;
-		((AutoStep) programSteps.get(0)).initialize();
-		s_log.logp(Level.FINE, "Auton", "Step Starting", programSteps.get(0).toString());
-	}
+   public void initialize()
+   {
+      defineSteps();
+      loadStopPosition();
+      currentStep = 0;
+      finishedPreviousStep = false;
+      finished = false;
+      ((AutoStep) programSteps.get(0)).initialize();
+      s_log.logp(Level.FINE, "Auton", "Step Starting", programSteps.get(0).toString());
+   }
 
-	public void cleanup() {
-		programSteps.clear();
-	}
+   public void cleanup()
+   {
+      programSteps.clear();
+   }
 
-	public void update() {
-		if (finished) {
-			return;
-		}
-		if (finishedPreviousStep) {
-			finishedPreviousStep = false;
-			currentStep++;
-			if (currentStep >= programSteps.size()) {
-				finished = true;
-				return;
-			} else {
-			   s_log.logp(Level.FINE, "Auton", "Step Start", programSteps.get(currentStep).toString());
-				((AutoStep) programSteps.get(currentStep)).initialize();
-			}
-		}
-		AutoStep step = (AutoStep) programSteps.get(currentStep); // Prevent
-																				// errors
-																				// caused
-																				// by
-																				// mistyping.
-		step.update();
-		if (step.isFinished()) {
-		   s_log.logp(Level.FINE, "Auton", "Step Finished", step.toString());
-			finishedPreviousStep = true;
-		}
-	}
+   public void update()
+   {
+      if (finished)
+      {
+         return;
+      }
+      if (finishedPreviousStep)
+      {
+         finishedPreviousStep = false;
+         currentStep++;
+         if (currentStep >= programSteps.size())
+         {
+            finished = true;
+            return;
+         }
+         else
+         {
+            s_log.logp(Level.FINE, "Auton", "Step Start", programSteps.get(currentStep).toString());
+            ((AutoStep) programSteps.get(currentStep)).initialize();
+         }
+      }
+      AutoStep step = (AutoStep) programSteps.get(currentStep); // Prevent errors caused by mistyping.
+      SmartDashboard.putString("Current auto step", step.toString());
+      step.update();
+      if (step.isFinished())
+      {
+         s_log.logp(Level.FINE, "Auton", "Step Finished", step.toString());
+         finishedPreviousStep = true;
+      }
+   }
 
-	public AutoStep getCurrentStep() {
-		return (AutoStep) programSteps.get(currentStep);
-	}
+   public AutoStep getCurrentStep()
+   {
+      return (AutoStep) programSteps.get(currentStep);
+   }
 
-	public AutoStep getNextStep() {
-		if (currentStep + 1 < programSteps.size()) {
-			return (AutoStep) programSteps.get(currentStep + 1);
-		} else {
-			return null;
-		}
-	}
+   public AutoStep getNextStep()
+   {
+      if (currentStep + 1 < programSteps.size())
+      {
+         return (AutoStep) programSteps.get(currentStep + 1);
+      }
+      else
+      {
+         return null;
+      }
+   }
 
-	protected void loadStopPosition() {
-		int forceStopAtStep = Core.getConfigManager().getConfig().getInt(this.getClass().getName() + ".ForceStopAtStep", 0);
-		if (forceStopAtStep != 0) {
-			int forceStop = forceStopAtStep;
-			if ((forceStop <= programSteps.size()) && (forceStop > 0)) {
-				programSteps.set(forceStop, new AutoStepStopAutonomous());
-				s_log.logp(Level.ALL, "Auton", "Force Stop", "Program is forced to stop at Step " + forceStop);
-			} else {
-			   s_log.logp(Level.SEVERE, "Auton", "Force Stop", "Force stop value is outside of bounds. (0 to " + (programSteps.size() - 1));
-			}
-		}
-	}
+   protected void loadStopPosition()
+   {
+      int forceStopAtStep = Core.getConfigManager().getConfig().getInt(this.getClass().getName() + ".ForceStopAtStep", 0);
+      if (forceStopAtStep != 0)
+      {
+         int forceStop = forceStopAtStep;
+         if ((forceStop <= programSteps.size()) && (forceStop > 0))
+         {
+            programSteps.set(forceStop, new AutoStepStopAutonomous());
+            s_log.logp(Level.ALL, "Auton", "Force Stop", "Program is forced to stop at Step "
+                  + forceStop);
+         }
+         else
+         {
+            s_log.logp(Level.SEVERE, "Auton", "Force Stop", "Force stop value is outside of bounds. (0 to "
+                  + (programSteps.size() - 1));
+         }
+      }
+   }
 
-	public boolean isFinished() {
-		return finished;
-	}
+   public boolean isFinished()
+   {
+      return finished;
+   }
 
-	protected void addStep(AutoStep newStep) {
-		programSteps.add(newStep);
-	}
+   protected void addStep(AutoStep newStep)
+   {
+      programSteps.add(newStep);
+   }
 
-	public abstract String toString();
+   public abstract String toString();
 }
